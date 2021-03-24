cc93120f219178021b267ee0466eb34f08d1f55c
Raka Fajar S <raka.fajar@gits.id>
2021-03-24 16:50:00 +0700

    Config API List AlQuran

--- lib/network/api_repository.dart
+++ lib/network/api_repository.dart
@@ -1,5 +1,6 @@
 import 'package:meta/meta.dart';
+import 'package:my_alquran/model/m_list_alquran.dart';
 
 abstract class Repository {
-  // Future<ModelListMovie> readListMovie();
+  Future<ModelListAlQuran> getListAlQuran();
 }
--- lib/network/api_service.dart
+++ lib/network/api_service.dart
@@ -1,26 +1,26 @@
 import 'package:dio/dio.dart';
 import 'package:meta/meta.dart';
+import 'package:my_alquran/model/m_list_alquran.dart';
 import 'package:my_alquran/network/api_repository.dart';
 import 'package:my_alquran/network/dio_client.dart';
+import 'package:my_alquran/utils/extensions.dart';
 
 class ApiService implements Repository {
   Response response;
 
   Dio get dio => dioClient();
 
-  // @override
-  // Future<ModelListMovie> readListMovie() async {
-  //   try {
-  //     response = await dio.get(
-  //       'https://api.themoviedb.org/3/movie/popular?api_key=0b0debe0535354fd45b3b86a81fd35a2&language=en-US&page=1&region=',
-  //     );
-  //     return ModelListMovie.fromJson(response.data);
-  //   } on DioError catch (e) {
-  //     throw e.getErrorMessage();
-  //   } catch (error, stacktrace) {
-  //     throw showException(error, stacktrace);
-  //   }
-  // }
-
-
+  @override
+  Future<ModelListAlQuran> getListAlQuran()  async{
+      try {
+        response = await dio.get(
+          'surah',
+        );
+        return ModelListAlQuran.fromJson(response.data);
+      } on DioError catch (e) {
+        throw e.getErrorMessage();
+      } catch (error, stacktrace) {
+        throw showException(error, stacktrace);
+      }
+  }
 }
