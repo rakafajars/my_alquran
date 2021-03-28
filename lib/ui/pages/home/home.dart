54178daaf351dfc3ea0c53ef28d1e263a3bfaabb
raka fajar <rakafajar18@gmail.com>
2021-03-28 17:11:06 +0700

    Add Menu SVG Picture

--- lib/ui/pages/home/home.dart
+++ lib/ui/pages/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:my_alquran/config/route_name.dart';
 import 'package:my_alquran/theme/theme_color.dart';
 import 'package:my_alquran/theme/theme_text.dart';
@@ -77,19 +78,35 @@ class _HomePagesState extends State<HomePages> {
                       scrollDirection: Axis.horizontal,
                       children: [
                         menuButton(
-                          image: 'image/quran.png',
+                          image: 'image/quran.svg',
                           titleName: "Al-Qur'an",
+                          onTap: () {
+                            Navigator.pushNamed(
+                              context,
+                              RouteName.listAlQuran,
+                            );
+                          },
                         ),
                         menuButton(
-                          image: 'image/praying.png',
+                          image: 'image/quran.svg',
+                          titleName: "Hadits",
+                          onTap: () {
+                            Navigator.pushNamed(
+                              context,
+                              RouteName.listHadits,
+                            );
+                          },
+                        ),
+                        menuButton(
+                          image: 'image/praying.svg',
                           titleName: "Jadwal\nSholat",
                         ),
                         menuButton(
-                          image: 'image/doa.png',
+                          image: 'image/doa.svg',
                           titleName: "Doa-Doa",
                         ),
                         menuButton(
-                          image: 'image/masjid.png',
+                          image: 'image/masjid.svg',
                           titleName: "Masjid\nTerdekat",
                         ),
                       ],
@@ -98,51 +115,6 @@ class _HomePagesState extends State<HomePages> {
                 ],
               ),
             ),
-            // body: Column(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     Row(
-            //       mainAxisAlignment: MainAxisAlignment.center,
-            //       children: [
-            //         GestureDetector(
-            //           onTap: () {
-            //             Navigator.pushNamed(
-            //               context,
-            //               RouteName.listAlQuran,
-            //             );
-            //           },
-            //           child: Container(
-            //             height: sy(120),
-            //             width: sy(120),
-            //             child: Card(
-            //               color: Colors.transparent,
-            //               child: Image.asset('image/al-quran.png'),
-            //             ),
-            //           ),
-            //         ),
-            //         InkWell(
-            //           onTap: () {
-            //             Navigator.pushNamed(
-            //               context,
-            //               RouteName.listHadits,
-            //             );
-            //           },
-            //           child: Container(
-            //             height: sy(120),
-            //             width: sy(120),
-            //             child: Card(
-            //               child: Center(
-            //                 child: Text(
-            //                   'Hadits',
-            //                 ),
-            //               ),
-            //             ),
-            //           ),
-            //         ),
-            //       ],
-            //     ),
-            //   ],
-            // ),
           ),
         );
       },
@@ -152,6 +124,7 @@ class _HomePagesState extends State<HomePages> {
   Widget menuButton({
     String image,
     String titleName,
+    VoidCallback onTap,
   }) {
     return RelativeBuilder(
       builder: (context, height, width, sy, sx) {
@@ -162,25 +135,32 @@ class _HomePagesState extends State<HomePages> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: sy(72),
-                width: sy(72),
-                decoration: BoxDecoration(
-                  color: whiteColor,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(
-                      8.0,
+              GestureDetector(
+                onTap: onTap,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: whiteColor,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            8.0,
+                          ),
+                        ),
+                      ),
+                      width: sy(72),
+                      height: sy(72),
                     ),
-                  ),
-                ),
-                child: Container(
-                  child: Image.asset(
-                    image,
-                    width: sy(42),
-                    height: sy(42),
-                  ),
+                    SvgPicture.asset(
+                      image,
+                      width: sy(42),
+                      height: sy(42),
+                    ),
+                  ],
                 ),
               ),
+              // ),
               Container(
                 padding: EdgeInsets.only(
                   top: 4,
